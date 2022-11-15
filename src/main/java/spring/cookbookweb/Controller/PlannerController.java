186d12e1b02c22e2da7f80d0654ec4626bdc974c
Tobias J <tobias.jaks@gmail.com>
2022-11-15 20:59:32 +0100

    backend weekly planner working

--- src/main/java/spring/cookbookweb/Controller/PlannerController.java
+++ src/main/java/spring/cookbookweb/Controller/PlannerController.java
@@ -1,9 +1,14 @@
 package spring.cookbookweb.Controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import spring.cookbookweb.Entity.Recipe;
+import spring.cookbookweb.Services.PlannerService;
 
 @Controller
 public class PlannerController {
@@ -18,12 +23,13 @@ public class PlannerController {
     @GetMapping("/generated-planner")
     public String showWeeklyPlanner(Model model, 
     @RequestParam("mealType") String[] mealType,
-    @RequestParam("portions") int[] portions,
-    @RequestParam("maxTime") int[] maxCookTime){
+    @RequestParam("portions") String[] portions,
+    @RequestParam("maxTime") String[] maxCookTime){
         
-        for(String meal : mealType){
-            System.out.println(meal);
-        }
+        List<Recipe> weeklyPlan = PlannerService.calculateIngredientMeasures(PlannerService.getFromDB(mealType, maxCookTime), portions);
+        
+        
+        model.addAttribute("recipes", weeklyPlan);
         return "weekly-planner";
     }
 
--- src/main/java/spring/cookbookweb/Services/PlannerService.java
+++ src/main/java/spring/cookbookweb/Services/PlannerService.java
@@ -10,19 +10,40 @@ import spring.cookbookweb.Entity.Recipe;
 @Service
 public class PlannerService {
 
-    SearchService mySearch;
+    static SearchService mySearch;
 
     public PlannerService(SearchService search) {
-        this.mySearch = search;
+        PlannerService.mySearch = search;
     }
 
-    //method to retrieve recipies
-    public void getFromDB(String[] mealtypes, int[] maxCookTime){
+    //method to retrieve recipies from DB
+    public static List<Recipe> getFromDB(String[] mealtypes, String[] maxCookTime){
         List<Recipe> generated = new ArrayList<>();
 
-        for(String meal : mealtypes){
-            
+        for(int i = 0; i < mealtypes.length; i++){
+            List<Recipe> temporary = mySearch.searchCookBook(mealtypes[i], maxCookTime[i], "");
+            generated.add(GeneratorRecipeService.generateOneDish(temporary));
         }
+        
+        return generated;
+    }
+
+    // changing amount in each ingredients to users desired portion amount
+    public static List<Recipe> calculateIngredientMeasures(List<Recipe> generatedRecipes, String[] portions){
+
+        for(int a = 0; a < generatedRecipes.size(); a++){
+
+            // loop through each recipes all ingredients and change portion values
+            for(int i = 0; i < generatedRecipes.get(a).getIngredients().size(); i++){
+                generatedRecipes.get(a).getAmount().get(i).setAmount(
+                (generatedRecipes.get(a).getAmount().get(i).getAmount() / generatedRecipes.get(a).getPortions()) 
+                * Integer.parseInt(portions[a]) );
+            }
+
+            generatedRecipes.get(a).setPortions(Integer.parseInt(portions[a]));
+        }
+
+        return generatedRecipes;
     }
     
 
