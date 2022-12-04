eeea680e9b0d1c41147eb72559ff3757b2e96241
Tobias J <tobias.jaks@gmail.com>
2022-12-04 11:22:16 +0100

    remove security files to start over

--- src/main/java/spring/cookbookweb/config/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package spring.cookbookweb.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
-    
-    
-}
