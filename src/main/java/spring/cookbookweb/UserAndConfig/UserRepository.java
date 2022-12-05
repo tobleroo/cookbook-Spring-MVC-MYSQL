730b1a59a176ab84a12edb6e8dfc4802e1cf0367
Tobias J <tobias.jaks@gmail.com>
2022-12-05 19:47:54 +0100

    removed old config and user files

--- src/main/java/spring/cookbookweb/UserAndConfig/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package spring.cookbookweb.UserAndConfig;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<MyUser, Long>{
-    
-    MyUser findByUserName(String name);
-}
